bde75ccd1178d1ed25a0aae81da973eefa1ddc7a
Gonçalo Rações <goncalo_nr@hotmail.com>
2020-04-16 11:50:12 +0100

    Chapter 8, left at page 135

--- chapter-8.scm
+++ chapter-8.scm
@@ -5,3 +5,64 @@
     )
   )
 )
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+        ((null? l) '())
+        ((test? (car l) a) (cdr l))
+        (else
+          (cons
+            (car l)
+            ((rember-f test?) a (cdr l))))))))
+
+(define insert-g
+  (lambda (seq)
+    (lambda (new old l)
+      (cond
+        ((null? l) '())
+        ((eq? (car l) old)
+          (seq new old (cdr l)))
+        (else
+          (cons
+            (car l)
+            ((insert-g seq) new old (cdr l))
+          )
+        )
+      )
+    )
+  )
+)
+
+(define insertL
+  (insert-g
+    (lambda (new old l)
+      (cons new (cons old l))
+    )
+  )
+)
+
+(define atom-to-function
+  (lambda (atom)
+    (cond
+      ((eq? atom '+) +)
+      ((eq? atom 'x) x)
+      (else expt)
+    )
+  )
+)
+
+(define value
+  (lambda (nexp)
+    (cond
+      ((atom? nexp) nexp)
+      (else
+        ((atom-to-function (operator nexp))
+          (value (1st-sub-exp nexp))
+          (value (2nd-sub-exp nexp)))
+      )
+    )
+  )
+)
+
